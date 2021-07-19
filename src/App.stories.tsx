6a1037df4f8be7186c94c26f756113cb215c6a75
akky-xxxx <114.s.aki@gmail.com>
2021-07-19 11:28:21 +0900

    chore: setup storybook

---
+++ src/App.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import App from './App';
+
+export default {
+  title: 'App',
+  component: App,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof App>;
+
+const Template: ComponentStory<typeof App> = (args) => <App {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
